f5400652eb80dde490d82edc6b54ad62e595549f
Béla Kellner <bela.kellner@icloud.com>
2023-06-22 15:10:39 +0200

    Parkolási engedélyek felület módosítása
    - Minden név mellett mostantól szerepel a rendfokozat is (kivéve lenyíló menüknél)
    - Parkolási engedélyek mostantól tulajdonos alapján ABC sorrendben jelenik meg
    - Adatbázis mostantól helyi SQL szerverhez csatlakozik
    - Parkolási engedélynél mostantól 1 karakter is lehet a jármű típus
    - Rendfokozati névelírás javítás

---
+++ src/ISZR.Web/Migrations/20230622113802_202306221337.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ISZR.Web.Migrations
+{
+    public partial class _202306221337 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Report_Users_ReportUserId",
+                table: "Report");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Report",
+                table: "Report");
+
+            migrationBuilder.RenameTable(
+                name: "Report",
+                newName: "Reports");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Report_ReportUserId",
+                table: "Reports",
+                newName: "IX_Reports_ReportUserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Reports",
+                type: "nvarchar(48)",
+                maxLength: 48,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reports",
+                table: "Reports",
+                column: "ReportId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Users_ReportUserId",
+                table: "Reports",
+                column: "ReportUserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Users_ReportUserId",
+                table: "Reports");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reports",
+                table: "Reports");
+
+            migrationBuilder.RenameTable(
+                name: "Reports",
+                newName: "Report");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reports_ReportUserId",
+                table: "Report",
+                newName: "IX_Report_ReportUserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Report",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(48)",
+                oldMaxLength: 48,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Report",
+                table: "Report",
+                column: "ReportId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Report_Users_ReportUserId",
+                table: "Report",
+                column: "ReportUserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+    }
+}
