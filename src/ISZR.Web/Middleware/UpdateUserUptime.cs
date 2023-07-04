bd876a6eada6ff779e97b17d3270dd5314caaa19
Béla Kellner <bela.kellner@icloud.com>
2023-07-04 09:31:02 +0200

    Felhasználó aktivitásának frissítése
    - A rendszer amennyiben megtalálja az adott felhasználónevet az adatbázisban, annak utolsó megtekintési idejét frissíti minden oldal látógatás során

---
+++ src/ISZR.Web/Middleware/UpdateUserUptime.cs
@@ -0,0 +1,57 @@
+﻿namespace ISZR.Web.Middleware
+{
+    /// <summary>
+    /// Felhasználó utolsó megtekintési idejének módosítása az adatbázisban
+    /// </summary>
+    public class UpdateUserUptime : IMiddleware
+    {
+        private readonly DataContext _context;
+
+        public UpdateUserUptime(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Felhasználó megtekintési idejének módosítása az adatbázisban
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // Felhasználó megkeresése az adatbázisban
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Username == context.User.Identity.Name);
+
+            if (user != null)
+            {
+                try
+                {
+                    // Felhasználó idejének frissítése az aktuális idővel
+                    user.LastLogin = DateTime.Now;
+
+                    // Felhasználó értékeinek frissítése az adatbázisban
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (UserExists(user.UserId))
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            // A rendszer továbbléptetése
+            await next(context);
+        }
+
+        /// <summary>
+        /// Felhasználói azonosító megkeresése a rendszerben
+        /// </summary>
+        /// <param name="id">Felhasználói azonosító</param>
+        /// <returns>Létezik e a felhasználói azonosító (igaz/hamis)</returns>
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
