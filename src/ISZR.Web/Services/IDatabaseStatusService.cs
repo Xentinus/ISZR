c2b7addf53b98afd82f13933f85bad6480b59587
Béla Kellner <bela.kellner@icloud.com>
2023-07-26 14:28:46 +0200

    Állapotfelmérés felújítása
    - Állapotmelmérés mostantól új felületen fog megjelenni ahol csak a fontosabb információk jelennek meg és azok i jobban érhetőek
    - Beépített életjel lett hozzáadva az alkalmazáshoz amelyet a /healthz felületen el lehet érni
    - Adatbázis állapotának ellenőrzés bevezetés az Állapotfelmérés oldalon
    - ViewModel alapértelmezett értékek beállítása

---
+++ src/ISZR.Web/Services/IDatabaseStatusService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ISZR.Web.Services
+{
+    /// <summary>
+    /// Adatbázis állapotának ellenőrzése (interface)
+    /// </summary>
+    public interface IDatabaseStatusService
+    {
+        bool IsDatabaseOnline();
+    }
+
+    /// <summary>
+    /// Adatbázis állapotának ellenőrzése
+    /// </summary>
+    public class DatabaseStatusService : IDatabaseStatusService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseStatusService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DataContext");
+        }
+
+        /// <summary>
+        /// Elérhető e a megadott adatbázis
+        /// </summary>
+        public bool IsDatabaseOnline()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
