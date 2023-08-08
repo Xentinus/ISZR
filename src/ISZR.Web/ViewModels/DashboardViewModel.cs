fdae0730f72d1ea1c8848d752a918ed62fba0fc9
Béla Kellner <bela.kellner@icloud.com>
2023-08-08 15:29:48 +0200

    Igénylés statisztika eltávolítása a profilból
    - Jelenlegi igénylés statisztika eltávolítása a felhasználói profilból
    - Többletjogosultság jogosultság számlálásának fixálása

--- src/ISZR.Web/ViewModels/DashboardViewModel.cs
+++ src/ISZR.Web/ViewModels/DashboardViewModel.cs
@@ -10,26 +10,6 @@ namespace ISZR.Web.ViewModels
         public User? User { get; set; }
 
         /// <summary>
-        /// Felhasználó által kért igénylések összesen
-        /// </summary>
-        public int AllRequests { get; set; } = 0;
-
-        /// <summary>
-        /// Felhasználó által kért igénylések amelyek végre lettek hajtva
-        /// </summary>
-        public int DoneRequests { get; set; } = 0;
-
-        /// <summary>
-        /// Felhasználó által kért igénylések amelyek még folyamatban vannak
-        /// </summary>
-        public int InProgressRequests { get; set; } = 0;
-
-        /// <summary>
-        /// Felhasználó által kért igénylések amelyek el lettek utasítva
-        /// </summary>
-        public int DeniedRequests { get; set; } = 0;
-
-        /// <summary>
         /// Felhasználó parkolási igényei
         /// </summary>
         public List<Parking>? Parkings { get; set; } = new List<Parking>();
