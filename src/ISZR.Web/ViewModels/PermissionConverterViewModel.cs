76b3a843010503a45856b0afdcd73569e889b69e
Béla Kellner <bela.kellner@icloud.com>
2023-08-29 15:55:04 +0200

    Jogosultság konvertálás lehetősége
    - Mostantól az Active Directory-ból szedett felhasználó jogosultságokat vagy szolgálati jegyről másolt jogosultságokat egy helyen át lehet konvertálni ISZR jogosultsággá, azaz tudjuk hogy melyek azok a jogosultságok amelyekről szó van és az ISZR tartalmazza

---
+++ src/ISZR.Web/ViewModels/PermissionConverterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ISZR.Web.Models;
+
+namespace ISZR.Web.ViewModels
+{
+    public class PermissionConverterViewModel
+    {
+        public string UserInput { get; set; } = string.Empty;
+        public HashSet<Permission> FoundPermissions { get; set; } = new HashSet<Permission>();
+        public List<string> NotFoundPermissions { get; set; } = new List<string>();
+
+    }
+}
